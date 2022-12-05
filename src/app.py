32430aff9469d06403ae38487801a40875398283
Chase Struse <chasestruse@gmail.ocm>
2022-12-05 14:20:21 -0600

    implemented logic for app

--- src/app.py
+++ src/app.py
@@ -6,7 +6,74 @@ load_dotenv()
 key = os.getenv('API_KEY')
 api_service = ApiService(key)
 
+
+def print_command_menu():
+    print('Please select from the list of commands below: \n')
+    print('1. Get parks in your state')
+    print('2. Get park and activities (Requires park name and state abbreviation)')
+    print('3. ')
+    print('4. ')
+
+
+def print_parks_and_descriptions():
+    state_abbrev = input('Please enter state abbreviation(ex: CA): ')
+    try:
+        name_description_dict = api_service.get_parks_by_state(state_abbrev)
+
+        print('____________________________')
+        print('\nList of National Parks')
+        print('____________________________')
+        for park, description in name_description_dict.items():
+            print(f'{park} \n{description} \n')
+        input('Press enter to continue')
+    except:
+        print('Please check input and try again \n')
+        print_parks_and_descriptions()
+
+
+def print_park_information():
+    try:
+        state_abbrev = input('Please enter state abbreviation(ex: CA): ')
+        park_name = input('Please enter park name: ')
+        park = api_service.get_park_by_name_and_state(state_abbrev, park_name)
+
+        print('____________________________')
+        print('Park Information')
+        print('____________________________')
+        print(f'{park.name} \n'
+              f'{park.description} \n')
+        print('List of Activities Available')
+        print('____________________________')
+        print(*park.activities, sep=', ')
+        print('____________________________\n')
+        input('Press enter to continue')
+    except:
+        print('Please check input and try again \n')
+        print_park_information()
+
+
+def main_loop():
+    keep_using = True
+    print('Welcome to National Parks Command Line Application \n')
+    print('__________________________________________________')
+    while keep_using:
+        try:
+            print_command_menu()
+            selection = int(input('Enter selection: '))
+
+            if selection == 1:
+                print_parks_and_descriptions()
+
+            elif selection == 2:
+                print_park_information()
+
+            elif selection == 0:
+                print('Thank you for using the application!')
+                keep_using = False
+
+        except ValueError:
+            print('Please ensure you are entering a number from the menu \n')
+
+
 if __name__ == '__main__':
-    name_description_dict = api_service.get_parks_by_state('CA')
-    for key, value in name_description_dict.items():
-        print(f'{key} \n{value} \n')
+    main_loop()
