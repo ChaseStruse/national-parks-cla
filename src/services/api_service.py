c15ef08340a083e0ba01046593a1d54a91f312cc
Chase Struse <chasestruse@gmail.ocm>
2022-12-06 17:09:19 -0600

    added call to api

--- src/services/api_service.py
+++ src/services/api_service.py
@@ -19,6 +19,15 @@ class ApiService:
         response = requests.get(url)
         return self.data_conversion_service.convert_parks_json_to_national_park_object(response.json(), park_name)
 
+    def get_parks_amenities_by_park_name_and_state(self, state_code, park_name):
+        park_name_url = park_name.replace(' ', '%20')
+        url = self.baseurl + f'/parks?limit=5&stateCode={state_code}&q={park_name_url}&api_key={self.api_key}'
+        response = requests.get(url)
+        park_code = self.data_conversion_service.get_park_code_from_parks_json(response.json(), park_name)
+        amen_url = self.baseurl + f'/amenities/parksplaces?parkCode={park_code}&api_key={self.api_key}'
+        amen_response = requests.get(amen_url)
+        return self.data_conversion_service.convert_park_amenities_json_to_list(amen_response.json())
+
 
 
 
