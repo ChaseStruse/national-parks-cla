c8502bd770ad56b4f3a5f6277c070572afa0111b
Chase Struse <chasestruse@gmail.ocm>
2022-12-06 17:07:58 -0600

    added tests for new endpoint

--- tests/services/tests_data_conversion_service.py
+++ tests/services/tests_data_conversion_service.py
@@ -3,6 +3,7 @@ import unittest
 from unittest import TestCase
 
 from src.classes.national_park import NationalPark
+from src.classes.national_park_amenity import NationalParkAmenity
 from src.services.data_conversion_service import DataConversionService
 
 
@@ -50,6 +51,26 @@ class TestDataConversionService(TestCase):
         self.assertEqual(expected.name, actual.name)
         self.assertEqual(expected.description, actual.description)
         self.assertEqual(expected.activities, actual.activities)
+
+    def test_given_one_park_get_park_code_from_parks_json_returns_correct_park_code(self):
+        test_json = '{ "total": "1", "data": [{ "fullName": "Test Park", "parkCode":"cOdE" }] }'
+        expected = 'cOdE'
+        actual = self._sut.get_park_code_from_parks_json(json.loads(test_json), 'Test Park')
+        self.assertEqual(expected, actual)
+
+    def test_given_two_parks_get_park_code_from_parks_json_returns_correct_park_code(self):
+        test_json = ('{ "total": "2", "data": [{ "fullName": "Test Park", "parkCode":"cOdE" },'
+                     '{ "fullName": "Test Park 2", "parkCode":"cOdEtWo" } ] }')
+        expected = 'cOdE'
+        actual = self._sut.get_park_code_from_parks_json(json.loads(test_json), 'Test Park')
+        self.assertEqual(expected, actual)
+
+    def test_given_park_amenity_json_convert_park_amenities_json_to_list_returns_correct_list(self):
+        test_json = ('{ "data": [ [{ "name": "amen1", "parks": [{ "places":[ { "title": "amenity title 1" }, { "title": "amenity title 2" }] }] }] ] }')
+        expected = [NationalParkAmenity('amen1', ['amenity title 1', 'amenity title 2'])]
+        actual = self._sut.convert_park_amenities_json_to_list(json.loads(test_json))
+        self.assertEqual(expected[0].name, actual[0].name)
+        self.assertEqual(expected[0].locations_available[0], actual[0].locations_available[0])
 
 
 if __name__ == '__main__':
